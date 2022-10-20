6a5a9817508707913fd152a2b4d4053bd6d58104
vjastoni <vjastoni13@gmail.com>
2022-10-20 22:30:42 +0800

    state change

--- src/adminDashBoard/pages/CaseFolders.jsx
+++ src/adminDashBoard/pages/CaseFolders.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, Fragment } from "react";
 import { storage, db } from "../../firebase";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import {
   collection,
   query,
@@ -20,34 +20,25 @@ export default function CaseFolders() {
   const [foldersList, setFoldersList] = useState([]);
   const fileNameRef = useRef();
   const folderNameRef = useRef();
+  const courtRef = useRef();
   const [folderOption, setFolderOption] = useState("");
   const [fileList, setFileList] = useState([]);
-  const { username, email, id } = UseUserReducer();
-  const [editState, setEditState] = useState(false);
+  const { username, id, initials } = UseUserReducer();
+  const [readState, setReadState] = useState(true);
+  const [share, setShare] = useState();
+  const [editShareId, setEditShareId] = useState();
+  const [editFormData, setEditFormData] = useState({});
 
-  const handleClick = async (folder) => {
+  const handleGetFiles = async () => {
     setFileList([]);
-    // foldersList?.map(async folder => {
-    //   console.log(folder)
-    //   const colRef = collection(db, `users/${id}/${folder}`)
-    //   const fileRef = query(colRef, where('author', '==', `${email}`))
-    //   await getDocs(fileRef).then(data => {
-    //     setFileList(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
-    //   })
-    // setFileList(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
-    // fileList?.map(file => console.log(file))
-    // })
-    const colRef = collection(db, `users/${id}/${folder}`);
-    const authorRef = query(colRef, where("author", "==", `${email}`));
+    const colRef = collection(db, `files`);
+    const authorRef = query(colRef, where("author", "==", `${initials}`));
     const shareRef = query(colRef, where("shareable", "==", true));
     const authorDocs = await getDocs(authorRef);
     const shareDocs = await getDocs(shareRef);
     const data1 = authorDocs.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     const data2 = shareDocs.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     setFileList([data1, data2]);
-    // setFileList(authorDocs.docs.map(doc => ({ ...doc.data(), id: doc.id })))
-    // setFileList({...authorDocs,})
-    // console.log(fileList)
   };
 
   const addFolder = () => {
@@ -61,22 +52,34 @@ export default function CaseFolders() {
     folderNameRef.current.value = "";
   };
 
-  const uploadFile = () => {
+  const uploadFile = async () => {
     setLoading(true);
-    if (fileUpload === null || folderOption === "") return;
+    if (folderOption === "") {
+      alert("Please select a folder.");
+      setLoading(false);
+      return;
+    }
+    if (fileUpload === null) {
+      alert("Please select a file.");
+      setLoading(false);
+      return;
+    }
     const extension = fileUpload.name.split(".").pop();
-    const fileUrl = `caseFiles/${username}/${folderOption}/file-${fileNameRef.current.value}.${extension}`;
+    const fileUrl = `caseFiles/${username}/${folderOption}/${fileNameRef.current.value}.${extension}`;
     const fileRef = ref(storage, fileUrl);
-    uploadBytes(fileRef, fileUpload).then((snapshot) => {
+    await uploadBytes(fileRef, fileUpload).then((snapshot) => {
       getDownloadURL(snapshot.ref).then(async (url) => {
         const data = {
+          active: true,
           filename: snapshot.ref.name,
-          author: email,
+          date_created: new Date().toString(),
+          author: initials,
           folder: folderOption,
           shareable: false,
           url: url,
+          court: courtRef.current.value,
         };
-        const colRef = collection(db, `users/${id}/${folderOption}`);
+        const colRef = collection(db, `files`);
         const docRef = doc(colRef);
         await setDoc(docRef, data).then(() => {
           console.log("successfully added file in firestore");
@@ -89,7 +92,48 @@ export default function CaseFolders() {
     setFileUpload(null);
     setFolderOption("");
     fileNameRef.current.value = "";
+    courtRef.current.value = "";
     setLoading(false);
+  };
+
+  const handleEditClick = (e, data) => {
+    e.preventDefault();
+    setEditShareId(data.id);
+
+    const formValues = {
+      shareable: data.shareable,
+    };
+    setEditFormData(formValues);
+    setReadState(false);
+  };
+
+  const handleEdit = (e) => {
+    const selectedOption = e.target.value;
+    setShare(selectedOption);
+  };
+
+  const handleCancel = () => {
+    setEditShareId(null);
+    setReadState(true);
+  };
+
+  const checkFolder = (folder1, folder2) => {
+    if (folder1 !== folder2) return;
+  };
+
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault();
+    const docRef = doc(db, `files`, editShareId);
+
+    const editedFile = {
+      shareable: share,
+    };
+
+    setDoc(docRef, editedFile, { merge: true }).then(() => {
+      alert("Document updated Successfully");
+    });
+    setReadState(true);
+    setEditShareId(null);
   };
 
   useEffect(() => {
@@ -101,55 +145,8 @@ export default function CaseFolders() {
     };
 
     getFolders();
-  }, [id]);
-
-  // useEffect(() => {
-  //   const getPrefixes = () => {
-  //     listAll(fileListRef).then(res => {
-  //       res.prefixes.forEach(prefix => {
-  //         setPrefixList(prefixList => [...prefixList, prefix.name])
-  //         listAll(prefix).then(res => {
-  //           res.items.forEach(item => {
-  //             getDownloadURL(item).then(url => {
-  //               const data = {
-  //                 prefix: prefix.name,
-  //                 name: item.name,
-  //                 url: url,
-  //               }
-  //               setFileList([...fileList, data])
-  //             })
-  //           })
-  //         })
-  //       })
-  //     })
-  //   }
-  //   getPrefixes()
-  // }, [])
-
-  // const getFiles = prefix => {
-  //   const listRef = ref(storage, `caseFiles/${username}/${prefix}`)
-  //   listAll(listRef).then(res => {
-  //     res.items.forEach(item => {
-  //       getDownloadURL(item).then(url => {
-  //         setFileList(fileList => [...fileList, url])
-  //       })
-  //     })
-  //   })
-  // }
-
-  // const getFiles = () => {
-  //   listAll(fileListRef).then(res => {
-  //     res.prefixes.forEach(prefix => {
-  //       listAll(prefix).then(res => {
-  //         res.items.forEach(item => {
-  //           getDownloadURL(item).then(url => {
-  //             setFileList(fileList => [...fileList, url])
-  //           })
-  //         })
-  //       })
-  //     })
-  //   })
-  // }
+    handleGetFiles();
+  }, []);
 
   return (
     <div className="h-screen w-screen overflow-auto lg:overflow-hidden scrollbar-hide md:w-screen md:h-screen lg:w-screen lg:ml-48">
@@ -186,6 +183,11 @@ export default function CaseFolders() {
                   ))}
                 </select>
                 <input
+                  type="text"
+                  placeholder="Enter Court..."
+                  ref={courtRef}
+                />
+                <input
                   type="file"
                   onChange={(e) => setFileUpload(e.target.files[0])}
                 />
@@ -203,42 +205,54 @@ export default function CaseFolders() {
               </div>
               <div className="flex flex-col gap-10 lg:ml-10">
                 {foldersList.map((folder) => (
-                  <details className="mt-5">
-                    <summary
-                      className="cursor-pointer"
-                      onClick={() => handleClick(folder)}
-                    >
-                      {folder}
-                    </summary>
-                    {fileList?.map(
-                      (file) =>
-                        // console.log('file: ', file)
-                        file?.map((data) =>
-                          data.folder === folder ? (
-                            <a href={data.url}>{data.filename}</a>
-                          ) : (
-                            ""
-                          )
-                        )
-                      // return file.folder === folder ? <a href={file.url}>{file.filename}</a> : ''
-                    )}
-                  </details>
+                  <form onSubmit={handleEditFormSubmit}>
+                    <details className="mt-5">
+                      <summary
+                        className="cursor-pointer"
+                        onClick={() => handleGetFiles(folder)}
+                      >
+                        {folder}
+                      </summary>
+                      {fileList?.map((file) =>
+                        file?.map((data) => (
+                          <Fragment key={data.id}>
+                            {() => checkFolder(folder, data.folder)}
+                            {data.folder === folder ? (
+                              readState ? (
+                                <ReadOnlyRow
+                                  filename={data.filename}
+                                  shareable={data.shareable}
+                                  url={data.url}
+                                  date_created={data.date_created}
+                                  court={data.court}
+                                  initials={initials}
+                                  data={data}
+                                  handleEditClick={handleEditClick}
+                                  folder={data.folder}
+                                />
+                              ) : (
+                                <EditRow
+                                  editFormData={editFormData}
+                                  filename={data.filename}
+                                  shareable={data.shareable}
+                                  url={data.url}
+                                  date_created={data.date_created}
+                                  court={data.court}
+                                  initials={initials}
+                                  data={data}
+                                  handleCancel={handleCancel}
+                                  handleEdit={handleEdit}
+                                />
+                              )
+                            ) : (
+                              ""
+                            )}
+                          </Fragment>
+                        ))
+                      )}
+                    </details>
+                  </form>
                 ))}
-                {/* <details className='mt-5'>
-                  <summary className='cursor-pointer'>288-2-01 (NGCP vs. Perez)</summary>
-
-                  {fileList.map(file => {
-                    return <a href={file}>Download now</a>
-                  })}
-                </details>
-                <details>
-                  <summary className='cursor-pointer'>288-1-02 (NGCP vs. Tominez)</summary>
-                  <p></p>
-                </details>
-                <details>
-                  <summary className='cursor-pointer'>288-1-03 (NGCP vs. Villanueva)</summary>
-                  <p></p>
-                </details> */}
               </div>
             </div>
           </div>
@@ -268,5 +282,116 @@ export default function CaseFolders() {
         </div>
       </div>
     </div>
+  );
+}
+
+function ReadOnlyRow({
+  filename,
+  url,
+  shareable,
+  date_created,
+  initials,
+  court,
+  handleEditClick,
+  data,
+  folder,
+}) {
+  return (
+    <>
+      <table className="w-full ">
+        <thead>
+          <tr className="flex justify-around ">
+            <th className={`text-left w-1/5`}>Case No.</th>
+            <th className={`text-left w-1/5`}>Handling Associate</th>
+            <th className={`text-left w-1/5`}>Court</th>
+            <th className={`text-left w-1/5`}>Date Created</th>
+            <th className={`text-left w-1/5`}>Shareable</th>
+            <th className={`text-left w-1/5`}>Folder</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="flex justify-around">
+            <td className={`text-left w-1/5`}>
+              <a href={url}>{filename}</a>
+            </td>
+            <td className={`text-left w-1/5`}>{initials}</td>
+            <td className={`text-left w-1/5`}>{court}</td>
+            <td className={`text-left w-1/5`}>{date_created}</td>
+            <td className={`text-left w-1/5`}>
+              {shareable ? "Shared" : "Unshared"}
+            </td>
+            <td>{folder}</td>
+            <button
+              onClick={(e) => handleEditClick(e, data)}
+              className="w-14 h-8 rounded-md border-0 bg-maroon text-white"
+            >
+              Edit
+            </button>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+function EditRow({
+  filename,
+  url,
+  shareable,
+  date_created,
+  initials,
+  court,
+  handleCancel,
+  handleEdit,
+}) {
+  return (
+    <>
+      <table className="w-full ">
+        <thead>
+          <tr className="flex justify-around ">
+            <th className={`text-left w-1/5`}>Case No.</th>
+            <th className={`text-left w-1/5`}>Handling Associate</th>
+            <th className={`text-left w-1/5`}>Court</th>
+            <th className={`text-left w-1/5`}>Date Created</th>
+            <th className={`text-left w-1/5`}>Shareable</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="flex justify-around">
+            <td className={`text-left w-1/5`}>
+              <a href={url}>{filename}</a>
+            </td>
+            <td className={`text-left w-1/5`}>{initials}</td>
+            <td className={`text-left w-1/5`}>{court}</td>
+            <td className={`text-left w-1/5`}>{date_created}</td>
+            <select onChange={handleEdit}>
+              {shareable ? (
+                <>
+                  <option value={true}>Shared</option>
+                  <option value={false}>Unshared</option>
+                </>
+              ) : (
+                <>
+                  <option value={false}>Unshared</option>
+                  <option value={true}>Shared</option>
+                </>
+              )}
+            </select>
+            <button
+              className="w-14 h-8 rounded-md border-0 bg-maroon text-white"
+              type="submit"
+            >
+              Save
+            </button>
+            <button
+              onClick={handleCancel}
+              className="w-14 h-8 rounded-md border-0 bg-maroon text-white"
+            >
+              Cancel
+            </button>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 }
