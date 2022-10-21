8cdabaab3a35069f2761ea9a41549bea38cd688e
vjastoni <vjastoni13@gmail.com>
2022-10-21 13:41:43 +0800

    adds designs to edit profile

--- src/ownerDashboard/OwnerSideNavBar.jsx
+++ src/ownerDashboard/OwnerSideNavBar.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import { UserAuth } from '../context/AuthContext'
-import { ACTIONS } from './reducers/OwnerReducer'
+import React from "react";
+import { UserAuth } from "../context/AuthContext";
+import { ACTIONS } from "./reducers/OwnerReducer";
 
 function OwnerSideNavBar({ dispatch, hideNavBar }) {
-  const { logOut } = UserAuth()
+  const { logOut } = UserAuth();
 
   const handleSignOut = async () => {
     try {
-      await logOut()
+      await logOut();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const viewProfile = () => {
-    dispatch({ type: ACTIONS.VIEW_PROFILE })
-  }
+    dispatch({ type: ACTIONS.VIEW_PROFILE });
+  };
   const viewDashboard = () => {
-    dispatch({ type: ACTIONS.VIEW_DASHBOARD })
-  }
+    dispatch({ type: ACTIONS.VIEW_DASHBOARD });
+  };
   const viewLawyers = () => {
-    dispatch({ type: ACTIONS.VIEW_LAWYERS })
-  }
+    dispatch({ type: ACTIONS.VIEW_LAWYERS });
+  };
   const viewClients = () => {
-    dispatch({ type: ACTIONS.VIEW_CLIENTS })
-  }
+    dispatch({ type: ACTIONS.VIEW_CLIENTS });
+  };
   const viewFolders = () => {
-    dispatch({ type: ACTIONS.VIEW_CASEFOLDERS })
-  }
+    dispatch({ type: ACTIONS.VIEW_CASEFOLDERS });
+  };
   const viewTimesheets = () => {
-    dispatch({ type: ACTIONS.VIEW_TIMESHEETS })
-  }
+    dispatch({ type: ACTIONS.VIEW_TIMESHEETS });
+  };
   const viewAppointments = () => {
-    dispatch({ type: ACTIONS.VIEW_APPOINTMENTS })
-  }
+    dispatch({ type: ACTIONS.VIEW_APPOINTMENTS });
+  };
 
   return (
     <div>
@@ -42,108 +42,114 @@ function OwnerSideNavBar({ dispatch, hideNavBar }) {
       {/* Navigation Bar */}
       <div
         className={`${
-          hideNavBar ? 'left-[-300px]' : 'left-0'
+          hideNavBar ? "left-[-300px]" : "left-0"
         } fixed z-[1000] top-0 xl:left-0 h-screen w-[91px] m-0 flex flex-col items-center bg-[#d9d9d9] shadow-lg transition-all duration-300 `}
       >
-        <div className='fixed z-10'>
+        <div className="fixed z-10">
           <div
             onClick={viewProfile}
-            className='relative flex items-center justify-center h-16 w-16 mt-5 mb-10 mx-auto 
+            className="relative flex items-center justify-center h-16 w-16 mt-5 mb-10 mx-auto 
     shadow-lg bg-[#632121] hover:bg-[#ab940b]
-    rounded-xl hover:rounded-3xl transition-all duration-300 ease-linear cursor-pointer'
+    rounded-xl hover:rounded-3xl transition-all duration-300 ease-linear cursor-pointer"
           >
-            {<img alt='user icon' className='w-14 h-14' src={require('../assets/user.png')} />}
+            {
+              <img
+                alt="user icon"
+                className="w-14 h-14"
+                src={require("../assets/user.png")}
+              />
+            }
           </div>
         </div>
-        <div className=' h-screen w-sreen overflow-scroll scrollbar-hide mt-28 mb-28'>
-          {' '}
+        <div className=" h-screen w-sreen overflow-scroll scrollbar-hide mt-28 mb-28">
+          {" "}
           <SideBarIcon
             icon={
               <img
-                alt='dashboard icon'
+                alt="dashboard icon"
                 onClick={viewDashboard}
-                className='w-10 h-10 invert'
-                src={require('../assets/dashboard.jpg')}
+                className="w-10 h-10 invert"
+                src={require("../assets/dashboard.jpg")}
               />
             }
           />
-          <p className='text-center text-sm pt-0 mb-3 font-semibold'>Dashboard</p>
+          <p className="text-center text-sm pt-0 mb-3 ">Dashboard</p>
           <SideBarIcon
             icon={
               <img
-                alt='case folder icon'
+                alt="case folder icon"
                 onClick={viewLawyers}
-                className='w-10 h-10'
-                src={require('../assets/caseFolder.png')}
+                className="w-10 h-10"
+                src={require("../assets/caseFolder.png")}
               />
             }
           />
-          <p className='text-center text-sm pt-0 mb-3 font-semibold'>Lawyers</p>
+          <p className="text-center text-sm pt-0 mb-3 ">Lawyers</p>
           <SideBarIcon
             icon={
               <img
-                alt='client icon'
+                alt="client icon"
                 onClick={viewClients}
-                className='w-8 h-8'
-                src={require('../assets/client.jpg')}
+                className="w-8 h-8"
+                src={require("../assets/client.jpg")}
               />
             }
           />
-          <p className='text-center text-sm pt-0 mb-3 font-semibold'>Client</p>
+          <p className="text-center text-sm pt-0 mb-3 ">Client</p>
           <SideBarIcon
             icon={
               <img
-                alt='case folder icon'
+                alt="case folder icon"
                 onClick={viewFolders}
-                className='w-10 h-10'
-                src={require('../assets/caseFolder.png')}
+                className="w-10 h-10"
+                src={require("../assets/caseFolder.png")}
               />
             }
           />
-          <p className='text-center text-sm pt-0 mb-3 font-semibold'>Case Folder</p>
+          <p className="text-center text-sm pt-0 mb-3 ">Case Folder</p>
           <SideBarIcon
             icon={
               <img
-                alt='timesheet icon'
+                alt="timesheet icon"
                 onClick={viewTimesheets}
-                className='w-8 h-8'
-                src={require('../assets/timesheets.png')}
+                className="w-8 h-8"
+                src={require("../assets/timesheets.png")}
               />
             }
           />
-          <p className='text-center text-[13px] pt-0 mb-3 font-semibold'>Timesheet</p>
+          <p className="text-center text-[13px] pt-0 mb-3 ">Timesheet</p>
           <SideBarIcon
             icon={
               <img
-                alt='appointment'
+                alt="appointment"
                 onClick={viewAppointments}
-                className='w-8 h-8 invert'
-                src={require('../assets/appointment.png')}
+                className="w-8 h-8 invert"
+                src={require("../assets/appointment.png")}
               />
             }
           />
-          <p className='text-center text-[13px] pt-0 mb-3 font-semibold'>Appointment</p>
+          <p className="text-center text-[13px] pt-0 mb-3 ">Appointment</p>
         </div>
-        <div className='text-center fixed bottom-4 z-10'>
+        <div className="text-center fixed bottom-4 z-10">
           {
             <img
-              alt='signout'
+              alt="signout"
               onClick={handleSignOut}
-              className='w-8 h-8 invert cursor-pointer'
-              src={require('../assets/logout.png')}
+              className="w-8 h-8 invert cursor-pointer"
+              src={require("../assets/logout.png")}
             />
           }
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-const SideBarIcon = ({ icon, text = 'tooltip' }) => (
-  <div className='sidebar-icon group'>
+const SideBarIcon = ({ icon, text = "tooltip" }) => (
+  <div className="sidebar-icon group">
     {icon}
     {/* <span className="sidebar-tooltip group-hover:scale-100">{text}</span> */}
   </div>
-)
+);
 
-export default OwnerSideNavBar
+export default OwnerSideNavBar;
