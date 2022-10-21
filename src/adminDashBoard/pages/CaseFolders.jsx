0292c60a948f1222d68a91dbb755e9be2efc6469
vjastoni <vjastoni13@gmail.com>
2022-10-21 16:24:46 +0800

    adds modal input to casefolder

--- src/adminDashBoard/pages/CaseFolders.jsx
+++ src/adminDashBoard/pages/CaseFolders.jsx
@@ -150,6 +150,8 @@ export default function CaseFolders() {
     handleGetFiles();
   }, []);
 
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="h-screen w-screen overflow-auto lg:overflow-hidden scrollbar-hide md:w-screen md:h-screen lg:w-screen lg:ml-48">
       <h1 className="self-start text-[30px] mt-3 ml-5 font-bold">Case Files</h1>
@@ -159,52 +161,98 @@ export default function CaseFolders() {
           {/* Welcome Text */}
           <div className=" rounded-md  flex flex-col  bg-[#D9D9D9] lg:h-[87%] overflow-auto scrollbar-hide">
             <div className="m-5 lg:m-5 text-justify">
-              <h1 className="self-start text-[30px] font-bold">
+              <h1 className="self-start text-2xl font-bold">
                 {username}'s Case Folders
               </h1>
-              <div className="flex items-center justify-evenly">
-                <input
-                  type="text"
-                  ref={fileNameRef}
-                  placeholder="Enter filename..."
-                />
-                <select
-                  name="folders"
-                  id="folders"
-                  value={folderOption}
-                  onChange={(e) => {
-                    setFolderOption(e.target.value);
-                    console.log(e.target.value);
+              {/* INPUTS */}
+              <div className="flex justify-end items-center gap-2 mt-3 mr-2 ">
+                <button
+                  type="button"
+                  onClick={() => {
+                    setShowModal(true);
                   }}
+                  className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out"
                 >
-                  <option default value="">
-                    -Select Folder-
-                  </option>
-                  {foldersList?.map((folder) => (
-                    <option value={`${folder}`}>{folder}</option>
-                  ))}
-                </select>
-                <input
-                  type="text"
-                  placeholder="Enter Court..."
-                  ref={courtRef}
-                />
-                <input
-                  type="file"
-                  onChange={(e) => setFileUpload(e.target.files[0])}
-                />
-                <button disabled={loading} onClick={uploadFile}>
-                  Upload
+                  Add File
                 </button>
+                {showModal && (
+                  <div className="flex flex-col justify-center items-center bg-[#9C9999] absolute top-36 left-6 h-[60%] w-[85%] gap-5 rounded-md lg:h-[80%] lg:top-20 lg:left-[110px] lg:w-[68%] ">
+                    <div className="flex flex-col items-center gap-2">
+                      <h1 className="font-bold text-2xl">ADD FILE</h1>
+                      <hr className="w-64" />
+                    </div>
+                    <div className="flex flex-col items-center justify-evenly gap-5 mt-5">
+                      <input
+                        className="bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
+                        shadow appearance-none border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        ref={fileNameRef}
+                        placeholder="Enter filename..."
+                      />
+                      <select
+                        className="bg-white self-center border-black outline-none border-b-[1px] 
+                        shadow border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="folders"
+                        id="folders"
+                        value={folderOption}
+                        onChange={(e) => {
+                          setFolderOption(e.target.value);
+                          console.log(e.target.value);
+                        }}
+                      >
+                        <option default value="">
+                          -Select Folder-
+                        </option>
+                        {foldersList?.map((folder) => (
+                          <option value={`${folder}`}>{folder}</option>
+                        ))}
+                      </select>
+                      <input
+                        className="bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
+                        shadow appearance-none border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        placeholder="Enter Court..."
+                        ref={courtRef}
+                      />
+                      <input
+                        className="bg-[#9C9999] self-center lg:h-[35px]
+                       rounded w-[70%] py-2 px-3 text-gray-700 "
+                        type="file"
+                        onChange={(e) => setFileUpload(e.target.files[0])}
+                      />
+                      <button
+                        className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out"
+                        disabled={loading}
+                        onClick={uploadFile}
+                      >
+                        Upload
+                      </button>
+                    </div>
+                    <div className="flex justify-center gap-2">
+                      <input
+                        className="bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
+                         shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        ref={folderNameRef}
+                        placeholder="Enter folder to add"
+                      />
+                      <button
+                        className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out"
+                        onClick={addFolder}
+                      >
+                        Add Folder
+                      </button>
+                    </div>
+                    <p
+                      className="text-maroon font-bold cursor-pointer hover:text-white"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </p>
+                  </div>
+                )}
               </div>
-              <div className="flex items-center justify-evenly">
-                <input
-                  type="text"
-                  ref={folderNameRef}
-                  placeholder="Enter folder to add..."
-                />
-                <button onClick={addFolder}>Add Folder</button>
-              </div>
+              {/* END OF INPUTS */}
               <div className="flex flex-col gap-10 lg:ml-10">
                 {foldersList.map((folder) => (
                   <form onSubmit={handleEditFormSubmit}>
