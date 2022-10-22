a2a453c3b88131c2a9e79feed4e6bfb37780572f
vjastoni <vjastoni13@gmail.com>
2022-10-22 23:37:57 +0800

    initial change for case file design

--- src/adminDashBoard/pages/CaseFolders.jsx
+++ src/adminDashBoard/pages/CaseFolders.jsx
@@ -144,165 +144,158 @@ export default function CaseFolders() {
   const [showModal, setShowModal] = useState(false)
 
   return (
-    <div className='h-screen w-screen overflow-auto lg:overflow-hidden scrollbar-hide md:w-screen md:h-screen lg:w-screen lg:ml-48'>
-      <h1 className='self-start text-[30px] mt-3 ml-5 font-bold'>Case Files</h1>
-      <div className='h-full flex flex-col gap-5 items-center  md:w-full md:h-full lg:w-full lg:h-full lg:flex lg:flex-row'>
-        {/* First Div */}
-        <div className='w-[95%] gap-5 mt-6 lg:w-[95%] lg:h-[100%] lg:ml-2'>
-          {/* Welcome Text */}
-          <div className=' rounded-md  flex flex-col  bg-[#D9D9D9] lg:h-[87%] overflow-auto scrollbar-hide'>
-            <div className='m-5 lg:m-5 text-justify'>
-              <h1 className='self-start text-2xl font-bold'>{username}'s Case Folders</h1>
-              {/* INPUTS */}
-              <div className='flex justify-end items-center gap-2 mt-3 mr-2 '>
-                <button
-                  type='button'
-                  onClick={() => {
-                    setShowModal(true)
-                  }}
-                  className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
-                >
-                  Add File
-                </button>
-                {showModal && (
-                  <div className='w-screen h-screen backdrop-blur-sm absolute top-0 left-0 flex justify-center items-center'>
-                    <div className='flex flex-col justify-center items-center bg-[#BABABA] absolute h-[65%] w-[90%] gap-5 shadow-lg rounded-md md:h-[55%] md:w-[70%] lg:h-[80%] lg:w-[40%] '>
-                      <div className='flex flex-col items-center gap-2'>
-                        <h1 className='font-bold text-2xl'>ADD FILE</h1>
-                        <hr className='w-64' />
-                      </div>
-                      <div className='flex flex-col items-center justify-evenly gap-5 mt-5'>
-                        <input
-                          className='bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
-                        shadow appearance-none border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                          type='text'
-                          ref={fileNameRef}
-                          placeholder='Enter filename...'
-                        />
-                        <select
-                          className='bg-white self-center border-black outline-none border-b-[1px] 
-                        shadow border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                          name='folders'
-                          id='folders'
-                          value={folderOption}
-                          onChange={e => {
-                            setFolderOption(e.target.value)
-                            console.log(e.target.value)
-                          }}
-                        >
-                          <option default value=''>
-                            -Select Folder-
-                          </option>
-                          {foldersList?.map(folder => (
-                            <option value={`${folder}`}>{folder}</option>
-                          ))}
-                        </select>
-                        <input
-                          className='bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
-                        shadow appearance-none border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                          type='text'
-                          placeholder='Enter Court...'
-                          ref={courtRef}
-                        />
-                        <input
-                          className='bg-[#BABABA] lg:h-[40px]
-                       rounded w-[70%] py-2 px-3 text-gray-700 '
-                          type='file'
-                          onChange={e => setFileUpload(e.target.files[0])}
-                        />
-                        <button
-                          className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
-                          disabled={loading}
-                          onClick={uploadFile}
-                        >
-                          Upload
-                        </button>
-                      </div>
-                      <div className='flex justify-center gap-2'>
-                        <input
-                          className='bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
-                         shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                          type='text'
-                          ref={folderNameRef}
-                          placeholder='Enter folder name'
-                        />
-                        <button
-                          className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
-                          onClick={addFolder}
-                        >
-                          Add Folder
-                        </button>
-                      </div>
-                      <p
-                        className='text-maroon font-bold cursor-pointer hover:text-white'
-                        onClick={() => setShowModal(false)}
-                      >
-                        Close
-                      </p>
-                    </div>
+    <div className='h-screen w-screen overflow-auto flex flex-col items-center overflow-x-hidden md:h-screen md:w-screen lg:w-screen '>
+      <h1 className='self-start text-[30px] mt-3 ml-5 font-bold lg:ml-28'>
+        {username}'s Case Files
+      </h1>
+      <div className='h-full w-full flex flex-col gap-5 overflow-auto p-5 overflow-x-hidden lg:overflow-hidden lg:w-screen lg:h-screen lg:flex lg:flex-row lg:pr-0 lg:mt-0'>
+        <div className='w-[100%] h-[1000%] shadow-lg bg-[#D9D9D9] rounded-md flex flex-col gap-2 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 '>
+          <div className='w-[100%] h-[100%] pl-5 pt-5 pr-5 flex flex-col gap-2 lg:w-[100%] overflow-auto scrollbar-hide'>
+            {foldersList?.map(folder => (
+              <form onSubmit={handleEditFormSubmit}>
+                <div className='bg-[#9C9999] flex items-center rounded-lg shadow-lg w-[100%]'>
+                  <details className='ml-2'>
+                    <summary
+                      className='cursor-pointer text-md uppercase pt-2 pl-2 font-bold h-[70px] '
+                      onClick={() => handleGetFiles()}
+                    >
+                      {folder}
+                    </summary>
+                    {fileList?.map(file =>
+                      file?.map(data => (
+                        <Fragment key={data.id}>
+                          {data.folder === folder ? (
+                            readState ? (
+                              <ReadOnlyRow
+                                filename={data.filename}
+                                shareable={data.shareable}
+                                url={data.url}
+                                date_created={data.date_created}
+                                court={data.court}
+                                initials={initials}
+                                data={data}
+                                handleEditClick={handleEditClick}
+                                folder={data.folder}
+                              />
+                            ) : (
+                              <EditRow
+                                editFormData={editFormData}
+                                filename={data.filename}
+                                shareable={data.shareable}
+                                url={data.url}
+                                date_created={data.date_created}
+                                court={data.court}
+                                initials={initials}
+                                data={data}
+                                handleCancel={handleCancel}
+                                handleEdit={handleEdit}
+                              />
+                            )
+                          ) : (
+                            ''
+                          )}
+                        </Fragment>
+                      ))
+                    )}
+                  </details>
+                </div>
+              </form>
+            ))}
+          </div>
+          <div className='h-[50px] flex flex-col justify-center item-center self-end mb-2 mr-5'>
+            <button
+              type='button'
+              onClick={() => {
+                setShowModal(true)
+              }}
+              className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
+            >
+              Add File
+            </button>
+            {showModal && (
+              <div className='w-screen h-screen backdrop-blur-sm absolute top-0 left-0 flex justify-center items-center'>
+                <div className='flex flex-col justify-center items-center bg-[#BABABA] absolute h-[65%] w-[90%] gap-5 shadow-lg rounded-md md:h-[55%] md:w-[70%] lg:h-[80%] lg:w-[40%] '>
+                  <div className='flex flex-col items-center gap-2'>
+                    <h1 className='font-bold text-2xl'>ADD FILE</h1>
+                    <hr className='w-64' />
                   </div>
-                )}
+                  <div className='flex flex-col items-center justify-evenly gap-5 mt-5'>
+                    <input
+                      className='bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
+                        shadow appearance-none border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      type='text'
+                      ref={fileNameRef}
+                      placeholder='Enter Filename'
+                    />
+                    <select
+                      className='bg-white self-center border-black outline-none border-b-[1px] 
+                        shadow border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      name='folders'
+                      id='folders'
+                      value={folderOption}
+                      onChange={e => {
+                        setFolderOption(e.target.value)
+                        console.log(e.target.value)
+                      }}
+                    >
+                      <option default value=''>
+                        -Select Folder-
+                      </option>
+                      {foldersList?.map(folder => (
+                        <option value={`${folder}`}>{folder}</option>
+                      ))}
+                    </select>
+                    <input
+                      className='bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
+                        shadow appearance-none border rounded w-[70%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      type='text'
+                      placeholder='Enter Court'
+                      ref={courtRef}
+                    />
+                    <input
+                      className='bg-[#BABABA] lg:h-[40px]
+                       rounded w-[70%] py-2 px-3 text-gray-700 '
+                      type='file'
+                      onChange={e => setFileUpload(e.target.files[0])}
+                    />
+                    <button
+                      className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
+                      disabled={loading}
+                      onClick={uploadFile}
+                    >
+                      Upload
+                    </button>
+                  </div>
+                  <div className='flex justify-center gap-2'>
+                    <input
+                      className='bg-white self-center border-black outline-none border-b-[1px] lg:h-[35px]
+                         shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      type='text'
+                      ref={folderNameRef}
+                      placeholder='Enter folder name'
+                    />
+                    <button
+                      className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
+                      onClick={addFolder}
+                    >
+                      Add Folder
+                    </button>
+                  </div>
+                  <p
+                    className='text-maroon font-bold cursor-pointer hover:text-white'
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </p>
+                </div>
               </div>
-              {/* END OF INPUTS */}
-              <div className='flex flex-col mt-2 gap-2'>
-                {foldersList?.map(folder => (
-                  <form onSubmit={handleEditFormSubmit}>
-                    <div className='bg-[#9C9999] flex items-center rounded-lg shadow-lg w-[100%]'>
-                      <details className='ml-2'>
-                        <summary
-                          className='cursor-pointer text-md uppercase font-bold lg:text-lg'
-                          onClick={() => handleGetFiles()}
-                        >
-                          {folder}
-                        </summary>
-                        {fileList?.map(file =>
-                          file?.map(data => (
-                            <Fragment key={data.id}>
-                              {data.folder === folder ? (
-                                readState ? (
-                                  <ReadOnlyRow
-                                    filename={data.filename}
-                                    shareable={data.shareable}
-                                    url={data.url}
-                                    date_created={data.date_created}
-                                    court={data.court}
-                                    initials={initials}
-                                    data={data}
-                                    handleEditClick={handleEditClick}
-                                    folder={data.folder}
-                                  />
-                                ) : (
-                                  <EditRow
-                                    editFormData={editFormData}
-                                    filename={data.filename}
-                                    shareable={data.shareable}
-                                    url={data.url}
-                                    date_created={data.date_created}
-                                    court={data.court}
-                                    initials={initials}
-                                    data={data}
-                                    handleCancel={handleCancel}
-                                    handleEdit={handleEdit}
-                                  />
-                                )
-                              ) : (
-                                ''
-                              )}
-                            </Fragment>
-                          ))
-                        )}
-                      </details>
-                    </div>
-                  </form>
-                ))}
-              </div>
-            </div>
+            )}
           </div>
         </div>
         {/* Second Div */}
-        <div className='w-[95%] h-[100%] lg:h-[100%] lg:w-[30%] lg:mt-5 lg:mr-24'>
-          <div className='flex flex-col gap-5 mb-5 lg:w-[95%] lg:h-[88.4%]'>
-            <div className=' flex flex-col items-start text-white bg-maroon rounded-md lg:h-[50%] '>
+        <div className='w-[100%] h-[100%] lg:h-[100%] lg:w-[30%]'>
+          <div className='flex flex-col gap-5 mb-5 lg:w-[95%] lg:h-[100%]'>
+            <div className=' flex flex-col items-start shadow-lg  text-white bg-maroon rounded-md lg:h-[50%] '>
               {/* Todo */}
               <div className='flex flex-col m-5 text-justify '>
                 <div className='flex'>
