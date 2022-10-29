2d6e50f39c92a098bbc6ac48dd4f7739a2d17627
vjastoni <vjastoni13@gmail.com>
2022-10-29 18:44:43 +0800

    updates admin management

--- src/adminDashBoard/pages/AdminsManagement.jsx
+++ src/adminDashBoard/pages/AdminsManagement.jsx
@@ -88,63 +88,77 @@ export default function AdminsManagement() {
     <>
       <div className='h-screen w-screen p-5'>
         <h1 className='self-start text-[30px] font-bold lg:ml-20'>Create Admin</h1>
-        <div className='mt-2 overflow-auto scrollbar-hide p-5 lg:ml-20 w-[100%] h-[100%] shadow-lg bg-[#D9D9D9] rounded-md lg:w-[94%] lg:h-[93%]'>
-          <div>
-            <input
-              className='w-1/2 py-2 my-2 shadow appearance-none border rounded px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              type='text'
-              placeholder='Enter Username, Email or Expertise...'
-              value={searchKeyword}
-              onChange={handleSearch}
-            />
-          </div>
-
-          <div className='mt-2 flex flex-col justify-center'>
-            <div className='flex self-start'>
-              <h1 className='font-semibold text-xl'>Admin Details</h1>
-            </div>
+        <div>
+          <input
+            className='w-3/4 py-2 my-2 shadow appearance-none border rounded px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline lg:w-1/2 lg:ml-[81px]'
+            type='text'
+            placeholder='Enter Username, Email or Expertise...'
+            value={searchKeyword}
+            onChange={handleSearch}
+          />
+        </div>
+        <div className='overflow-auto scrollbar-hide p-5 lg:ml-20 w-[100%] h-[88%] shadow-lg bg-[#D9D9D9] rounded-md lg:w-[94%] lg:h-[85%] md:h-[92%]'>
+          <div className='flex flex-col justify-center'>
             {/* ADMIN DETAILS */}
-            <div className='mt-2'>
+            <div>
               <form onSubmit={handleEditFormSubmit}>
-                <table className='w-full text-xs text-center lg:text-sm lg:ml-2 border-collapse border border-slate-500 mt-2 mb-2'>
-                  <thead className={`text-xs text-gray-700 `}>
-                    <tr>
-                      <th th scope='col' className='py-3 px-6 lg:text-sm   border border-slate-600'>
-                        Username
-                      </th>
-                      <th th scope='col' className='py-3 px-6 lg:text-sm   border border-slate-600'>
-                        Email Address
-                      </th>
-                      <th th scope='col' className='py-3 px-6 lg:text-sm   border border-slate-600'>
-                        Role
-                      </th>
-                      <th th scope='col' className='py-3 px-6 lg:text-sm   border border-slate-600'>
-                        Expertise
-                      </th>
-                    </tr>
-                  </thead>
-                  {/* {loading ? ( */}
-                  <tbody>
-                    {search(admins).map(admin => (
-                      <Fragment key={admin.id}>
-                        {editAdminId === admin.id ? (
-                          <AdminEditRow
-                            admin={admin}
-                            editFormData={editFormData}
-                            handleEdit={handleEdit}
-                            handleCancelClick={handleCancelClick}
-                          />
-                        ) : (
-                          <AdminReadOnlyRow
-                            admin={admin}
-                            handleEditClick={handleEditClick}
-                            searchKeyword={searchKeyword}
-                          />
-                        )}
-                      </Fragment>
-                    ))}
-                  </tbody>
-                </table>
+                <div className='overflow-x-auto relative bg-white shadow-lg sm:rounded-lg p-2'>
+                  <table className='w-full text-sm text-center text-gray-500 border-collapse border border-slate-500 mt-2 mb-2'>
+                    <thead className={`text-xs text-gray-700 `}>
+                      <tr>
+                        <th
+                          th
+                          scope='col'
+                          className='py-3 px-6 lg:text-sm   border border-slate-600'
+                        >
+                          Username
+                        </th>
+                        <th
+                          th
+                          scope='col'
+                          className='py-3 px-6 lg:text-sm   border border-slate-600'
+                        >
+                          Email Address
+                        </th>
+                        <th
+                          th
+                          scope='col'
+                          className='py-3 px-6 lg:text-sm   border border-slate-600'
+                        >
+                          Role
+                        </th>
+                        <th
+                          th
+                          scope='col'
+                          className='py-3 px-6 lg:text-sm   border border-slate-600'
+                        >
+                          Expertise
+                        </th>
+                      </tr>
+                    </thead>
+                    {/* {loading ? ( */}
+                    <tbody>
+                      {search(admins).map(admin => (
+                        <Fragment key={admin.id}>
+                          {editAdminId === admin.id ? (
+                            <AdminEditRow
+                              admin={admin}
+                              editFormData={editFormData}
+                              handleEdit={handleEdit}
+                              handleCancelClick={handleCancelClick}
+                            />
+                          ) : (
+                            <AdminReadOnlyRow
+                              admin={admin}
+                              handleEditClick={handleEditClick}
+                              searchKeyword={searchKeyword}
+                            />
+                          )}
+                        </Fragment>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </form>
             </div>
           </div>
