6fe5a1d10a98b358c0e52356558afe8be846b31c
vjastoni <vjastoni13@gmail.com>
2022-10-30 16:09:35 +0800

    fixes design in folder

--- src/lawyerDashboard/pages/ClientsList.jsx
+++ src/lawyerDashboard/pages/ClientsList.jsx
@@ -22,7 +22,7 @@ export default function ClientsList() {
     <div className='h-screen w-screen overflow-auto flex flex-col items-center overflow-x-hidden md:h-screen md:w-screen lg:w-screen '>
       <h1 className='self-start text-[30px] mt-3 ml-5 font-bold lg:ml-28'>Clients</h1>
       <div className='h-full flex flex-col mt-2 gap-5 overflow-auto p-5 overflow-x-hidden lg:overflow-hidden lg:w-screen lg:h-screen lg:flex lg:flex-row lg:pr-0 lg:mt-0'>
-        <div className='w-[100%] h-[1000%] shadow-lg bg-[#D9D9D9] rounded-md flex flex-col gap-5 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 pt-5 '>
+        <div className='w-[100%] h-[1000%] shadow-lg bg-[#D9D9D9] rounded-md flex flex-col gap-5 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 pt-5 mr-3'>
           <div className='w-[100%] flex gap-10 flex-wrap justify-center lg:w-[100%] lg:overflow-auto lg:scrollbar-hide'>
             {clients?.map(client => (
               <div className='shadow-lg bg-[#632121] w-32 h-32 rounded-2xl flex flex-col items-center justify-center mb-5 md:w-48 md:h-48 lg:w-60 lg:h-60'>
