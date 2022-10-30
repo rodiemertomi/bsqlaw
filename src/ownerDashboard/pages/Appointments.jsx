f2cac3775710348263a53f494f221437094bc71c
vjastoni <vjastoni13@gmail.com>
2022-10-30 15:42:46 +0800

    fixes design in appointment

--- src/ownerDashboard/pages/Appointments.jsx
+++ src/ownerDashboard/pages/Appointments.jsx
@@ -27,43 +27,62 @@ function Appointments() {
     <div className='h-screen w-screen overflow-auto flex flex-col items-center overflow-x-hidden md:h-screen md:w-screen lg:w-screen '>
       <h1 className='self-start text-[30px] mt-3 ml-5 font-bold lg:ml-28'>Appointments</h1>
       <div className='h-full w-full flex flex-col gap-5 overflow-auto p-5 overflow-x-hidden lg:overflow-hidden lg:w-screen lg:h-screen lg:flex lg:flex-row lg:pr-0 lg:mt-0'>
-        <div className='w-[100%] h-[1000%] shadow-lg bg-[#D9D9D9] rounded-md flex flex-col gap-2 items-center lg:w-[100%] lg:h-[100%] lg:ml-20 '>
-          <div className='w-[100%] h-[100%] pl-5 pt-5 pr-5 flex flex-wrap gap-2 justify-center lg:w-[100%] overflow-auto scrollbar-hide'>
+        <div className='w-[100%] h-[1000%] shadow-lg bg-[#D9D9D9] rounded-md flex flex-col gap-2 items-center lg:w-[100%] lg:h-[100%] lg:ml-20  lg:mr-3'>
+          <div className='w-[100%] pl-5 pt-5 pr-5 flex flex-wrap gap-3 justify-center lg:justify-center lg:w-[100%] overflow-auto scrollbar-hide'>
             {appointments?.map(appointment => (
               <div
                 key={appointment.id}
-                className='bg-[#9C9999] w-[100%] h-60 rounded-md pl-2 flex flex-col gap-2 '
+                className='bg-[#FFF] drop-shadow-lg p-2 w-[100%] rounded-md flex flex-col gap-2 '
               >
                 <div>
-                  <span className='font-bold text-2xl'> {appointment.eventName} </span>
+                  <span className='mb-4 pl-2 text-xl font-bold text-maroon leading-none '>
+                    {appointment.eventName}
+                  </span>
                 </div>
-                <div>
-                  <span className='font-bold'>Client:</span> {appointment.clientFirstName}{' '}
-                  {appointment.clientLastName}
-                </div>
-                <div>
-                  <span className='font-bold'>Event Desc:</span> {appointment.eventDesc}
-                </div>
-                <div>
-                  <span className='font-bold'>Event Time Start: </span>
-                  {appointment.eventTimeStart}
-                </div>
-                <div>
-                  <span className='font-bold'>Event Time End: </span>
-                  {appointment.eventTimeEnd}
-                </div>
-                <div>
-                  <span className='font-bold'>Event Date Start: </span>
-                  {formatDate(appointment.eventDateStart.toDate())}
-                </div>
-                <div>
-                  <span className='font-bold'>Event Date End: </span>
-                  {formatDate(appointment.eventDateEnd.toDate())}
+                <div className='overflow-x-auto relative shadow-lg rounded-lg '>
+                  <table className='w-full text-sm text-center text-gray-500 border border-gray '>
+                    <thead className='text-xs text-gray-700 uppercase bg-gray-50 '>
+                      <tr>
+                        <th scope='col' className='py-3 px-6 '>
+                          Event Description
+                        </th>
+                        <th scope='col' className='py-3 px-6'>
+                          Event Time Start
+                        </th>
+                        <th scope='col' className='py-3 px-6'>
+                          Event Time End
+                        </th>
+                        <th scope='col' className='py-3 px-6'>
+                          Event Date Start
+                        </th>
+                        <th scope='col' className='py-3 px-6'>
+                          Event Date End
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className='bg-white dark:bg-gray-900 dark:border-gray-700'>
+                        <td className='py-4 px-6 lg:w-[22%]'>{appointment.eventDesc}</td>
+                        <td class='py-4 px-6'>{appointment.eventTimeStart}</td>
+                        <td class='py-4 px-6'>{appointment.eventTimeEnd}</td>
+                        <td class='py-4 px-6'>{formatDate(appointment.eventDateStart.toDate())}</td>
+                        <td class='py-4 px-6'> {formatDate(appointment.eventDateEnd.toDate())}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className='p-2 flex items-center gap-2 text-sm'>
+                    <img
+                      className='h-8 w-8'
+                      src={require('../../assets/user.png')}
+                      alt='user icon'
+                    />
+                    <span className='font-bold uppercase'>{appointment.client}</span>
+                  </div>
                 </div>
               </div>
             ))}
           </div>
-          <div className='h-[50px] flex flex-col justify-center item-center self-end mb-2 mr-5'>
+          <div className='h-[50px] flex flex-col justify-center item-center self-end mr-5 mb-1'>
             <button
               className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-3xl shadow-md bg-maroon hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out'
               onClick={() => {
@@ -74,7 +93,7 @@ function Appointments() {
             </button>
           </div>
           {showAppointment && (
-            <div className='w-screen h-screen backdrop-blur-sm absolute top-0 left-0 flex justify-center items-center'>
+            <div className='w-screen h-screen bg-modalbg absolute top-0 left-0 flex justify-center items-center'>
               <Times closeShowAppointment={setShowAppoitnment} getData={getData} />
             </div>
           )}
--- src/ownerDashboard/pages/components/Times.jsx
+++ src/ownerDashboard/pages/components/Times.jsx
@@ -64,7 +64,7 @@ function Times({ closeShowAppointment, getData }) {
   }
 
   return (
-    <div className='flex rounded-md justify-center items-center flex-col  border-1 border-black shadow-lg bg-[#BABABA] rounded-r h-[63%] w-[90%] lg:w-[40%] lg:h-[92%]'>
+    <div className='flex rounded-md justify-center items-center flex-col  border-1 border-black shadow-lg bg-[#e1dfdf] rounded-r h-[63%] w-[90%] lg:w-[40%] lg:h-[92%] drop-shadow-lg'>
       <h1 className='font-bold text-2xl'>Set Appointment</h1>
       <form onSubmit={saveEvent} className='mt-5'>
         <div className='mt-5 flex flex-col justify-center items-center gap-5'>
@@ -157,7 +157,7 @@ function Times({ closeShowAppointment, getData }) {
             className='bg-maroon w-[40%] text-white font-bold py-2 px-4 rounded-3xl shadow-md hover:bg-white hover:text-black active:shadow-lg transition duration-150 ease-in-out text-center'
           />
           <p
-            className='self-center text-maroon font-bold cursor-pointer hover:text-white mt-8'
+            className='text-maroon text-sm cursor-pointer hover:text-black hover:font-bold mt-8'
             onClick={() => closeShowAppointment(false)}
           >
             Close
