a1e60e1b78d6b8b4c369d48059ff24676fc727a3
vjastoni <vjastoni13@gmail.com>
2022-11-09 00:00:52 +0800

    fixes design

--- src/clientDashboard/pages/LawyersList.jsx
+++ src/clientDashboard/pages/LawyersList.jsx
@@ -21,7 +21,7 @@ export default function LawyersList() {
       <div className='h-screen w-screen overflow-auto flex flex-col items-center overflow-x-hidden md:h-screen md:w-screen lg:w-screen '>
         <h1 className='self-start text-[30px] mt-5 ml-5 font-bold lg:ml-28'>Lawyer</h1>
         <div className='h-full flex flex-col gap-5 overflow-auto p-5 overflow-x-hidden lg:w-screen lg:h-screen lg:flex lg:flex-row lg:pr-0'>
-          <div className='w-[100%] h-[1000%] bg-[#D9D9D9] rounded-md flex flex-col mr-3 gap-5 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 '>
+          <div className='w-[100%] h-[1000%] bg-maroon rounded-md flex flex-col mr-3 gap-5 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 '>
             <div className='w-[100%] flex gap-x-10 pt-5 flex-wrap justify-center lg:w-[100%] lg:overflow-auto lg:scrollbar-hide'>
               {lawyers?.map(lawyer => (
                 <Fragment key={lawyer.id}>
@@ -38,7 +38,7 @@ export default function LawyersList() {
 
 function ReadLawyers({ lawyer }) {
   return (
-    <div className='bg-[#632121] w-[230px] h-[260px] lg:w-[260px] lg:h-[285px] shadow-lg rounded-2xl flex flex-col items-center justify-center mb-5 md:w-[230px] md:h-[260px] text-white gap-[5px]'>
+    <div className='bg-[#ECE4E4] w-[230px] h-[260px] lg:w-[260px] lg:h-[285px] shadow-lg rounded-2xl flex flex-col items-center justify-center mb-5 md:w-[230px] md:h-[260px] text-black font-semibold gap-[5px]'>
       <div className=' text-xs lg:text-sm w-full flex flex-col justify-center items-center'>
         <img
           alt='user'
@@ -49,13 +49,13 @@ function ReadLawyers({ lawyer }) {
               : `${lawyer.photoURL}`
           }
         />
-        <h1 className='text-yellow font-bold text-lg '>{`${lawyer.firstname} ${lawyer.lastname}`}</h1>
+        <h1 className='text-maroon font-bold text-lg '>{`${lawyer.firstname} ${lawyer.lastname}`}</h1>
         <h1>{`${lawyer.username}`}</h1>
         <h1>{`${lawyer.initials}`}</h1>
       </div>
       <div className='w-full bg-black shadow-lg h-[20%] flex flex-col items-center justify-start p-2 text-xs '>
         <h1 className='font-bold text-yellow'>List of Clients</h1>
-        <div className=' overflow-auto scrollbar-hide flex flex-col items-center justify-center h-full w-full'>
+        <div className=' overflow-auto scrollbar-hide flex flex-col items-center text-white justify-center h-full w-full'>
           {lawyer.clients?.map((client, i) => (
             <h1 key={i}>
               {client.firstname} {client.lastname}
--- src/ownerDashboard/pages/ClientsList.jsx
+++ src/ownerDashboard/pages/ClientsList.jsx
@@ -154,7 +154,7 @@ export default function ClientsList() {
       <div className='h-screen w-screen overflow-auto flex flex-col items-center overflow-x-hidden md:h-screen md:w-screen lg:w-screen '>
         <h1 className='self-start text-[30px] mt-3 ml-5 font-bold lg:ml-28'>Client</h1>
         <div className='h-full flex flex-col mt-2 gap-5 overflow-auto p-5 overflow-x-hidden lg:overflow-hidden lg:w-screen lg:h-screen lg:flex lg:flex-row lg:pr-3 lg:pt-0'>
-          <div className='w-[100%] h-[1000%] bg-[#D9D9D9] rounded-md flex flex-col gap-5 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 p-5 '>
+          <div className='w-[100%] h-[1000%] bg-maroon rounded-md flex flex-col gap-5 items-center lg:w-[130%] lg:h-[100%] lg:ml-20 p-5 '>
             <div className='w-[100%] flex gap-x-7 flex-wrap justify-center lg:w-[100%] lg:overflow-auto lg:scrollbar-hide'>
               {clientsList?.map((client, i) => (
                 <Fragment key={client.id}>
@@ -181,7 +181,7 @@ export default function ClientsList() {
 
 function ReadClients({ handleAppointClick, client, i }) {
   return (
-    <div className='bg-[#632121] w-[240px] h-[270px] lg:w-[260px] lg:h-[270px] shadow-lg rounded-2xl flex flex-col md:w-[220px] md:h-[270px] items-center justify-center text-white gap-1'>
+    <div className='bg-[#ECE4E4] w-[240px] h-[270px] lg:w-[260px] lg:h-[270px] shadow-lg rounded-2xl flex flex-col md:w-[220px] md:h-[270px] items-center justify-center text-white gap-1'>
       <div className=' text-xs lg:text-sm w-full flex flex-col justify-center items-center gap-1'>
         <img
           alt='user'
@@ -192,7 +192,7 @@ function ReadClients({ handleAppointClick, client, i }) {
               : `${client.photoURL}`
           }
         />
-        <h1 className='text-yellow mt-1 font-bold text-lg '>{`${client.username}`}</h1>
+        <h1 className='text-maroon mt-1 font-bold text-lg '>{`${client.username}`}</h1>
         <div className='w-full bg-black p-2 shadow-lg flex flex-col items-center justify-start text-xs gap-1'>
           {client.lawyer ? (
             <h1 className='text-white font-semibold text-center flex flex-col items-center justify-center'>
@@ -225,7 +225,7 @@ function EditClient({
   return (
     <div
       key={client.id}
-      className='bg-[#632121] w-[240px] h-[275px] lg:w-[260px] lg:h-[275px] shadow-lg rounded-2xl flex flex-col mb-5 md:w-[220px] md:h-[270px] text-white gap-1'
+      className='bg-[#ECE4E4] w-[240px] h-[275px] lg:w-[260px] lg:h-[275px] shadow-lg rounded-2xl flex flex-col mb-5 md:w-[220px] md:h-[270px] text-white gap-1'
     >
       <div className=' text-xs lg:text-sm w-full flex flex-col justify-center items-center gap-3'>
         <img
@@ -237,9 +237,9 @@ function EditClient({
               : `${client.photoURL}`
           }
         />
-        <h1 className='text-yellow'>{`${client.firstname} ${client.lastname}`}</h1>
+        <h1 className='text-maroon text-base font-semibold'>{`${client.firstname} ${client.lastname}`}</h1>
         <select
-          className='w-[55%] h-6 inline-block text-maroon font-medium text-xs leading-tight uppercase rounded shadow-md bg-white active:shadow-lg transition'
+          className='w-[55%] h-6 inline-block text-white font-medium text-xs leading-tight uppercase rounded shadow-md bg-maroon active:shadow-lg transition'
           name='appoint-lawyer'
           onChange={handleEdit}
         >
@@ -255,13 +255,13 @@ function EditClient({
         </select>
         <div className='flex gap-1'>
           <button
-            className='w-[55px] h-6 inline-block text-maroon font-medium text-xs leading-tight uppercase rounded shadow-md bg-white hover:bg-maroon hover:text-white active:shadow-lg transition duration-150 ease-in-out'
+            className='w-[55px] h-6 inline-block text-white font-medium text-xs leading-tight uppercase rounded shadow-md bg-maroon hover:bg-maroon hover:text-white active:shadow-lg transition duration-150 ease-in-out'
             onClick={e => handleEditFormSubmit(e, client)}
           >
             Save
           </button>
           <button
-            className='w-[55px] h-6 inline-block text-maroon font-medium text-xs leading-tight uppercase rounded shadow-md bg-white hover:bg-maroon hover:text-white active:shadow-lg transition duration-150 ease-in-out'
+            className='w-[55px] h-6 inline-block text-white font-medium text-xs leading-tight uppercase rounded shadow-md bg-maroon hover:bg-maroon hover:text-white active:shadow-lg transition duration-150 ease-in-out'
             onClick={handleCancelClick}
           >
             Cancel
